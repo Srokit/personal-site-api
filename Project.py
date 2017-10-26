25c9e299043290362e54d695aac0ff3cda805c8a
Stanley Rokita <srok35@gmail.com>
2017-10-26 12:49:55 -0400

    Skeleton of projects api in place

---
+++ Project.py
@@ -0,0 +1,11 @@
+from peewee import CharField
+from .BaseModel import BaseModel
+
+class Project(BaseModel):
+
+    name = CharField(max_length=100)
+    description = CharField(max_length=300)
+
+    def to_dict(self):
+        return {'name': self.name,
+            'description': self.description}
--- application.py
+++ application.py
@@ -3,8 +3,10 @@ from flask import g, request
 import jwt
 from validate_email import validate_email
 import json
+from flask_mail import Mail
 
 from .Email import Email
+from .Project import Project
 
 app = flask.Flask(__name__)
 
@@ -24,7 +26,27 @@ def put_email():
     else:
         return {'success': False, 'message': 'Invalid email'}
 
+@app.route('/project', methods=['PUT'])
+def put_project():
+    new_project = request.body.get('project')
+    project = Project.create(**new_project)
+    if project is not None:
+        return {'success': True}
+    else:
+        return {'success': False}
 
+@app.route('/project/all', methods=['GET'])
+def get_project_all():
+
+    projects = Project.get()
+    projects_as_dicts = [ project.to_dict() for project in projects ]
+    return {'success': True, 'projects': projects_as_dicts}
+
+@app.route('/project/{proj_id}', methods=['GET'])
+def get_project(proj_id):
+
+    project_as_dict = Project.get(proj_id).to_dict()
+    return {'success': True, 'project': project_as_dict}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
