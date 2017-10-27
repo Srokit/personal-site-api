c8bc781701651cb2a822ad4b75d80f562931cf3c
Stanley Rokita <srok35@gmail.com>
2017-10-26 20:05:58 -0400

    jsonify all api responses now

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import flask
-from flask import g, request
+from flask import g, request, jsonify
 import jwt
 from validate_email import validate_email
 import json
@@ -38,9 +38,9 @@ def put_email():
     email = request.body.get('email')
     if validate_email(email) and len(email) <= 100:
         Email.get_or_create(email=email)
-        return {'success': True}
+        return jsonify({'success': True})
     else:
-        return {'success': False, 'message': 'Invalid email'}
+        return jsonify({'success': False, 'message': 'Invalid email'})
 
 @app.route('/project', methods=['PUT'])
 def put_project():
@@ -52,22 +52,22 @@ def put_project():
     send_update_for_project(project, mailer, email_addresses)
 
     if project is not None:
-        return {'success': True}
+        return jsonify({'success': True})
     else:
-        return {'success': False}
+        return jsonify({'success': False})
 
 @app.route('/project/all', methods=['GET'])
 def get_project_all():
 
     projects = Project.select().where(True).execute()
     projects_as_dicts = [ project.to_dict() for project in projects ]
-    return {'success': True, 'projects': projects_as_dicts}
+    return jsonify({'success': True, 'projects': projects_as_dicts})
 
 @app.route('/project/{proj_id}', methods=['GET'])
 def get_project(proj_id):
 
     project_as_dict = Project.get(Project.id == proj_id).to_dict()
-    return {'success': True, 'project': project_as_dict}
+    return jsonify({'success': True, 'project': project_as_dict})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
